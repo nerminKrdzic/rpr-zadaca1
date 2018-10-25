8a14d2cc81d401ea587e4b875cf45c4be92b306f
Nermin Krdzic <nkrdzic1@etf.unsa.ba>
2018-10-25 20:51:53 +0200

    Popravio ChessPiece abstraktnu klasu

--- src/ba/unsa/etf/rpr/ChessPiece.java
+++ src/ba/unsa/etf/rpr/ChessPiece.java
@@ -2,8 +2,14 @@ package ba.unsa.etf.rpr;
 
 public abstract class ChessPiece {
     public static Color color = null;
+    protected String position = null;
     //methods realized in child classes
-    public abstract String getPosition();
-    public abstract Color getColor();
+    public String getPosition(){ return position; }
+    public Color getColor(){ return color; }
     public abstract void move(String position) throws IllegalArgumentException, IllegalChessMoveException;
+    public ChessPiece(String position, Color color){
+        if(!CheckPositionFormat.checkPositionFormat(position)) throw new IllegalArgumentException();
+        this.position = position;
+        this.color = color;
+    }
 }
